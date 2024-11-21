4f3e1a0a60ad15278e5ea02ea49201a50adc1c2b
Wermerson Macedo <wermersonm@gmail.com>
2024-11-20 23:13:45 -0300

    Adicionando a funcao que buscar o objeto pelo o ID

--- server.js
+++ server.js
@@ -2,38 +2,45 @@ import express from "express";
 
 const posts = [
   {
-    descricao: "uma foto teste",
-    imagem: "https://placecats.com/millie/300/150"
+    id: 1, descricao: "uma foto teste", imagem: "https://placecats.com/millie/300/150"
   },
   {
-    descricao: "uma foto de uma paisagem",
-    imagem: "https://placeimg.com/300/150/nature"
+    id: 2, descricao: "uma foto de uma paisagem", imagem: "https://placeimg.com/300/150/nature"
   },
   {
-    descricao: "foto de uma cidade à noite",
-    imagem: "https://placeimg.com/300/150/city"
+    id: 3, descricao: "foto de uma cidade à noite", imagem: "https://placeimg.com/300/150/city"
   },
   {
+    id: 4,
     descricao: "uma imagem de um cachorro",
     imagem: "https://placeimg.com/300/150/animals"
   },
   {
+    id: 5,
     descricao: "uma foto de comida deliciosa",
     imagem: "https://placeimg.com/300/150/food"
   },
   {
+    id: 6,
     descricao: "um retrato de uma pessoa sorrindo",
     imagem: "https://placeimg.com/300/150/people"
   }
 ];
 
 
+
 const app = express();
-app.use(express.json)
+app.use(express.json())
 app.listen(3000,()=>{
   console.log("Server listening...");
 });
 
-app.get("/api",(req, res) =>{
-  res.status(200).send("Boas vindas à imersão!");
-})
+function buscarPostPorID(id){
+  return posts.findIndex((posts)=>{
+    return posts.id === Number(id)
+  })
+}
+app.get("/posts/:id",(req, res) =>{
+  const index = buscarPostPorID(req.params.id)
+  res.status(200).json(posts[index]);
+});
